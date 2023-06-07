d2b15440a3e4a19bcaeafbcd909525825a56af8a
davideochoaa <davideochoaa@gmail.com>
2023-06-07 11:49:04 +0200

    ✅ Feat: Created Error animation

--- src/index.js
+++ src/index.js
@@ -17,6 +17,27 @@ form.addEventListener('submit', function(event) {
    }
 });
 
+mailInput.addEventListener('input', event => {
+   event.preventDefault();
+   let email = mailInput.value;
+   let error = document.querySelector('#error-message');
+   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+   if (regex.test(email)) {
+      console.log(`The email: ${email} is valid`);
+      mailInput.classList.remove('not-submitted');
+      mailInput.classList.add('submitted');
+      error.classList.remove('is-showed');
+      error.classList.add('not-showed');
+   } else {
+      console.log(`The email: ${email} is not valid`);
+      mailInput.classList.remove('submitted');
+      mailInput.classList.add('not-submitted');
+      error.classList.remove('visually-hidden');
+      error.classList.remove('not-showed');
+      error.classList.add('is-showed');
+   }
+})
+
 /* mailInput.addEventListener('click', function() {
    mailInput.style.borderColor = 'var(--pale-blue)';
 }) */

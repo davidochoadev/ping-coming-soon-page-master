e03c7e13773f192747a715cc88d6ff39bcce04c9
davideochoaa <davideochoaa@gmail.com>
2023-06-08 02:38:38 +0200

    ✅ Feat: Created Pop-up

--- src/index.js
+++ src/index.js
@@ -1,20 +1,59 @@
 const mailInput = document.querySelector('input');
-const submitButton = document.querySelector('button');
+const submitButton = document.querySelector('#submit-button');
 const form = document.querySelector('.form__container');
+const wrongPopUp = document.querySelector('#wrong-pop-up');
+const successfulPopUp = document.querySelector('#successful-pop-up');
+const wrongBtn = document.querySelector('#wrong-btn');
+const successfulBtn = document.querySelector('#successful-btn');
 
+let wrongInterval
+let successfulInterval
+let wrongTimeout;
+let successfulTimeout;
 form.addEventListener('submit', function(event) {
    event.preventDefault();
    let email = mailInput.value;
    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+   const wrongBar = document.getElementById('wrong-bar');
+   const successfulBar = document.getElementById('successful-bar');
+   wrongBar.value = 100;
+   successfulBar.value = 100;
+   clearInterval(wrongInterval);
+   clearInterval(successfulInterval);
+   clearTimeout(wrongTimeout);
+   clearTimeout(successfulTimeout);
+   wrongInterval = setInterval(() => {
+      wrongBar.value--;
+      if (wrongBar.value === 0) {
+        clearInterval(wrongInterval);
+      }
+    }, 50);
+
+    successfulInterval = setInterval(() => {
+      successfulBar.value--;
+      if (successfulBar.value === 0) {
+        clearInterval(successfulInterval);
+      }
+    }, 50);
+
+
    // Check if the email is valid
    if (regex.test(email)) {
-      console.log(`The email: ${email} is valid`);
       submitButton.classList.remove('btn__not-submitted');
-      submitButton.classList.add('btn__submitted');
+      successfulPopUp.classList.remove('hidden__alert');
+      wrongPopUp.classList.add('hidden__alert');
+      clearTimeout(successfulTimeout);
+      successfulTimeout = setTimeout(function() {
+         successfulPopUp.classList.add('hidden__alert');
+      },5000)
    } else {
-      console.log(`The email: ${email} is not valid`);
-      submitButton.classList.remove('btn__submitted');
       submitButton.classList.add('btn__not-submitted');
+      wrongPopUp.classList.remove('hidden__alert');
+      successfulPopUp.classList.add('hidden__alert');
+      clearTimeout(wrongTimeout);
+      wrongTimeout = setTimeout(function() {
+         wrongPopUp.classList.add('hidden__alert');
+      },5000)
    }
 });
 
@@ -37,6 +76,16 @@ mailInput.addEventListener('input', event => {
       error.classList.remove('not-showed');
       error.classList.add('is-showed');
    }
+})
+
+wrongBtn.addEventListener('click', function(event) {
+   event.preventDefault();
+   wrongPopUp.classList.add('hidden__alert');
+})
+
+successfulBtn.addEventListener('click', event => {
+   event.preventDefault();
+   successfulPopUp.classList.add('hidden__alert');
 })
 
 /* mailInput.addEventListener('click', function() {
